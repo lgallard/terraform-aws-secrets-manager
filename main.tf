6ef3acf911e20f8ca699f6d74a762bf144157dbf
Luis M. Gallardo D <lgallard@gmail.com>
2020-06-26 17:59:49 -0300

    Rename key map for secrets definition

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ locals {
       kms_key_id              = lookup(secret, "kms_key_id", null)
       policy                  = lookup(secret, "policy", null)
       recovery_window_in_days = lookup(secret, "recovery_window_in_days", var.recovery_window_in_days)
-      secret_string           = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string") : (lookup(secret, "secret_key_values", null) != null ? jsonencode(lookup(secret, "secret_key_values", {})) : null)
+      secret_string           = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string") : (lookup(secret, "secret_key_value", null) != null ? jsonencode(lookup(secret, "secret_key_value", {})) : null)
       secret_binary           = lookup(secret, "secret_string", null) == null ? lookup(secret, "secret_binary", null) : null
       tags                    = lookup(secret, "tags", {})
     }
@@ -80,7 +80,7 @@ locals {
       kms_key_id               = lookup(secret, "kms_key_id", null)
       policy                   = lookup(secret, "policy", null)
       recovery_window_in_days  = lookup(secret, "recovery_window_in_days", var.recovery_window_in_days)
-      secret_string            = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string") : (lookup(secret, "secret_key_values", null) != null ? jsonencode(lookup(secret, "secret_key_values", {})) : null)
+      secret_string            = lookup(secret, "secret_string", null) != null ? lookup(secret, "secret_string") : (lookup(secret, "secret_key_value", null) != null ? jsonencode(lookup(secret, "secret_key_value", {})) : null)
       secret_binary            = lookup(secret, "secret_string", null) == null ? lookup(secret, "secret_binary", null) : null
       rotation_lambda_arn      = lookup(secret, "rotation_lambda_arn", null)
       automatically_after_days = lookup(secret, "automatically_after_days", var.automatically_after_days)
