cf4d99686cb926033a919b46bff64cc2a00daf21
Luis M. Gallardo D <lgallard@gmail.com>
2020-06-26 17:12:03 -0300

    Add rotate secret support in variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-variable "default_recovery_window_in_days" {
-  description = "A valid JSON document representing a resource policy."
+variable "recovery_window_in_days" {
+  description = "Specifies the number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days."
   type        = number
   default     = 30
+}
+
+# Secrets
+variable "rotate_secrets" {
+  description = "List of secrets to keep and rotate in AWS Secrets Manager"
+  type        = any
+  default     = []
 }
 
 # Secrets
@@ -11,10 +18,15 @@ variable "secrets" {
   default     = []
 }
 
+variable "automatically_after_days" {
+  description = "Specifies the number of days between automatic scheduled rotations of the secret."
+  type        = number
+  default     = 30
+}
+
 # Tags
 variable "tags" {
   description = "Specifies a key-value map of user-defined tags that are attached to the secret."
   type        = any
   default     = {}
 }
-
