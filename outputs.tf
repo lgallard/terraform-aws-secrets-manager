9356743295133afce769e21925f04484cd6d7ea6
Luis M. Gallardo D <lgallard@gmail.com>
2021-08-26 15:57:32 -0300

    Fix secret map outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "secret_ids" {
   description = "Secret ids map"
-  value       = { for k, v in aws_secretsmanager_secret.sm : k => v if k == "id" }
+  value       = { for k, v in aws_secretsmanager_secret.sm : k => v["id"] }
 }
 
 output "secret_arns" {
   description = "Secrets arns map"
-  value       = { for k, v in aws_secretsmanager_secret.sm : k => v if k == "arn" }
+  value       = { for k, v in aws_secretsmanager_secret.sm : k => v["arn"] }
 }
 
 # Rotate secrets
 output "rotate_secret_ids" {
   description = "Rotate secret ids map"
-  value       = { for k, v in aws_secretsmanager_secret.rsm : k => v if k == "id" }
+  value       = { for k, v in aws_secretsmanager_secret.rsm : k => v["id"] }
 }
 
 output "rotate_secret_arns" {
   description = "Rotate secret arns map"
-  value       = { for k, v in aws_secretsmanager_secret.rsm : k => v if k == "arn" }
+  value       = { for k, v in aws_secretsmanager_secret.rsm : k => v["arn"] }
 }
