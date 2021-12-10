b1e1d35f5dabe7e2041ec54e5668e0641a01d16f
root <root@ip-172-31-1-32.ec2.internal>
2021-12-10 02:22:52 +0000

    file updated

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "recovery_window_in_days" {
   description = "Specifies the number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days."
   type        = number
-  default     = {}
+  default     = 7
 }
 
 #Secrets
